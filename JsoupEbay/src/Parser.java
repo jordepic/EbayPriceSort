864cb21384f3dda0e87c763bcfa5948788f9a2bb
Jordan Epstein <jordepstein@gmail.com>
2018-02-24 14:29:04 -0500

    Price and shipping work properly
    Also included change to classpath so that the jar is included in teh project file itself.

--- JsoupEbay/src/Parser.java
+++ JsoupEbay/src/Parser.java
@@ -17,16 +17,16 @@ public class Parser {
 		System.out.println("Please enter your ebay URL to sort by price with shiping costs! (You better enter the right link)");
 		Scanner getLink = new Scanner (System.in);
 		String myLink = getLink.nextLine();
-		System.out.println("Please enter the maximum price that you would be willing to pay (just an Double amount).");
+		System.out.println("Please enter the maximum price that you would be willing to pay (just a double amount).");
 		double myMax = Double.parseDouble(getLink.nextLine());
 		
 		try {
 			org.jsoup.nodes.Document doc = Jsoup.connect(myLink).userAgent("Chrome/63.0.3239.132").get();
 			Elements title = doc.select("h3.lvtitle");
 			Elements base = doc.select("li.lvprice");
-			Elements ship = doc.select("span.ship");
+			Elements ship = doc.select("li.lvshipping");
 			for (Element number: base) {
-				basePrice.add(number.getElementsByTag("span").first().text());
+				basePrice.add(number.getElementsByTag("span").first().getElementsByTag("span").first().text());
 				
 			}
 			for (Element number: ship) {
@@ -36,12 +36,13 @@ public class Parser {
 			//Could be impossible to parse due to $
 			//https://stackoverflow.com/questions/16787099/how-to-split-the-string-into-string-and-integer-in-java use this to break string up
 			
-			for (int i = 0; i < shipPrice.size() - 1; i++) {
+			for (int i = 0; i < shipPrice.size(); i++) {
 				if (shipPrice.get(i).replaceAll("[\\D]", "").equals("")) {
 					finalPrice.add(Double.parseDouble(basePrice.get(i).replaceAll("[\\D]", ""))/100.);
 				}
 				else {
 					finalPrice.add(Double.parseDouble(basePrice.get(i).replaceAll("[\\D]", ""))/100. + Double.parseDouble(shipPrice.get(i).replaceAll("[\\D]", ""))/100.);
+					
 				}
 				
 			}
